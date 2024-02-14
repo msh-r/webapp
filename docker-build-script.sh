678f53944b3f3bb2e173d26bba00893faad17008
msh-r <154027637+msh-r@users.noreply.github.com>
2024-02-14 05:02:01 -0500

    Create docker-build-script.sh

---
+++ docker-build-script.sh
@@ -0,0 +1,10 @@
+REMOTE_USER="ubuntu"                        # Username on the remote EC2 instance
+REMOTE_HOST="3.89.27.8"    # IP address or DNS name of the remote EC2 instance
+DOCKERFILE_PATH="/home/ubuntu/webapp"  # Path to your Dockerfile on the remote EC2 instance
+
+# SSH into the remote EC2 instance and execute Docker build
+ssh -i /home/ubuntu/.ssh/id_rsa $REMOTE_USER@$REMOTE_HOST << EOF
+  echo "Connected to remote instance."
+  cd $(dirname $DOCKERFILE_PATH)
+  docker build -t tomcat_image $(basename $DOCKERFILE_PATH)
+EOF
