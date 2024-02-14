79cb2eda868b02222493a827dc921bb280e4a036
msh-r <154027637+msh-r@users.noreply.github.com>
2024-02-14 05:38:05 -0500

    Update docker-build-script.sh

--- docker-build-script.sh
+++ docker-build-script.sh
@@ -2,6 +2,7 @@ REMOTE_USER="ubuntu"                        # Username on the remote EC2 instanc
 REMOTE_HOST="3.89.27.8"    # IP address or DNS name of the remote EC2 instance
 DOCKERFILE_PATH="/home/ubuntu/webapp"  # Path to your Dockerfile on the remote EC2 instance
 
+#docker run -d --name tomcat-container -p 8081:8090 tomcat-image
 # SSH into the remote EC2 instance and execute Docker build
 sudo -u ubuntu ssh -i /home/ubuntu/.ssh/id_rsa $REMOTE_USER@$REMOTE_HOST << EOF
   echo "Connected to remote instance."
@@ -10,5 +11,4 @@ sudo -u ubuntu ssh -i /home/ubuntu/.ssh/id_rsa $REMOTE_USER@$REMOTE_HOST << EOF
   docker stop $(docker ps -aq) && docker rm $(docker ps -aq) 
   docker build -t tomcat_image $(basename $DOCKERFILE_PATH)
   pwd
-  docker run -d --name tomcat-container -p 8081:8090 tomcat-image
-EOF
+  EOF
